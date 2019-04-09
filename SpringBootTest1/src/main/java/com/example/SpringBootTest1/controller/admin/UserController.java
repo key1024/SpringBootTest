3ff5162d692acb6eb1e6309c376a04bbcd3832bb
key1024 <479733293@qq.com>
2019-04-09 22:32:38 +0800

    提交文件上传控制器

---
+++ SpringBootTest1/src/main/java/com/example/SpringBootTest1/controller/admin/UserController.java
@@ -0,0 +1,21 @@
+package com.example.SpringBootTest1.controller.admin;
+
+import com.example.SpringBootTest1.entity.User;
+import com.example.SpringBootTest1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/findAll")
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+}
